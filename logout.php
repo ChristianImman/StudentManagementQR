5984508413f92e50f419558341f8ae0f14d03451
ChristianImman <114922181+ChristianImman@users.noreply.github.com>
2025-04-21 11:05:05 +0800

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-echo "Logout script reached."; // Debugging line
-session_destroy();
-header("Location: index.php"); // Adjust this path based on your actual structure
-exit();
-?>
